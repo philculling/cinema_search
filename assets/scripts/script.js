87f3af7dbdebf5a13bebbb0bb4eedec15f7d455b
Piers Ebdon <37227905+piers12@users.noreply.github.com>
2023-02-09 18:54:55 +0000

    add search button event listener and validate input

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,2 +1,18 @@
 var ytApi = "AIzaSyCbq2wbLQScvSCu8bJhd4ByuojcF55ekzo"; 
 var omdbApi = "";
+
+function searchButtonListener() {
+    $("#search-button").on("click", function() {
+    
+        // get text from input field and remove white space before and after
+        var searchInput = $("#film-search").val().trim();
+    
+        // validate it is not an empty string
+        if (!searchInput) {
+            console.log("Empty String");
+            return;
+        }
+    })
+}
+
+searchButtonListener();
