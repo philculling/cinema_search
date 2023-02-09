fd5aa3f8744bf36d029b125430be4885d81b017e
Phil Culling <philculling@hotmail.com>
2023-02-09 19:22:52 +0000

    set up queryURL for OMDB API, console log references for extracting data, create variable for image

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,2 +1,27 @@
 var ytApi = "AIzaSyCbq2wbLQScvSCu8bJhd4ByuojcF55ekzo"; 
-var omdbApi = "";
+var omdbApi = "d5cced46";
+
+//for testing:
+var userInput = "Titanic";
+
+var queryURLomdbapi = "http://www.omdbapi.com/?t=" + userInput + "&apikey=d5cced46"
+
+$.ajax({
+    url: queryURLomdbapi,
+    method: "GET"
+  }).then(function (response) {
+    //tests, all are fine
+    console.log(response.Title);
+    console.log(response.Year);
+    console.log(response.Rated);
+    console.log(response.Released);
+    console.log(response.Runtime);
+    console.log(response.Plot);
+    console.log(response.BoxOffice);
+    console.log(response.Poster);
+
+    var imgURL = response.Poster;
+    var image = $("<img>").attr("src", imgURL);
+
+  })
+
