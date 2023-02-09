4af118131f26593b5b9c106ba552669763270870
Nai <nailahmukhtar@Nailahs-Air.home>
2023-02-09 20:17:44 +0000

    added function to call youtube api

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -24,17 +24,37 @@ $.ajax({
   })
 }
 
+//Youtube API Call -- this needs to run when the play clip button is clicked - need a play button and event listener on that button
+function callYoutubeApi() {
+    var film = searchInput + " trailer";
+    var ytQueryUrl =
+    `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=${film}&type=video&videoDuration=short&videoEmbeddable=true&key=${ytApi}`;
+    
+    $.ajax({
+        url: ytQueryUrl,
+        method: "GET"
+    }).then(function (response) {
+        var videoId = response.items[0].id.videoId;
+        var videoTitle = response.items[0].snippet.title;
+        var videoURL = `https://www.youtube.com/watch?v=${videoId}`;
+        // console.log(videoTitle);
+        // console.log(videoURL);
+
+    });
+}
+
+
 function searchButtonListener() {
     $("#search-button").on("click", function() {
     
         // get text from input field and remove white space before and after
         searchInput = $("#film-search").val().trim();
-    
         // validate input is not an empty string
         if (!searchInput) {
             return;
         }
         callOmdbApi();
+        
     })
 }
 
