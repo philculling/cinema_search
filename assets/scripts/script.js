3869cb07918924120942625c838490be2655528b
Nai <nailahmukhtar@Nailahs-Air.home>
2023-02-16 20:15:19 +0000

    scrolling recents

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -177,10 +177,32 @@ function displayLocalStorageOnInitialLoad() {
     recentSearchesHeaderEl.insertBefore("#history");
 
     storedSearches.forEach(element => {
-        const liElement = $("<button>");
-        liElement.addClass("list-group-item list-group-item-action");
-        liElement.text(element.title);
-        $("#history").append(liElement);
+
+        const colDiv = $("<div>");
+        colDiv.addClass("col-auto")
+
+        const cardEl = $("<div>");
+        cardEl.addClass("card card-block");
+        cardEl.css("width", "200px");
+        cardEl.css("height", "400px");
+
+        const imgEl = $("<img>");
+        imgEl.addClass("card-img-top");
+        imgEl.attr("src", element.imageURL);
+
+        const cardBodyEl = $("<div>");
+        cardBodyEl.addClass("card-body");
+
+        const cardTitleEl = $("<p>");
+        cardTitleEl.addClass("card-text");
+        cardTitleEl.text(element.title);
+
+        $("#history").append(colDiv);
+        colDiv.append(cardEl);
+        cardEl.append(imgEl);
+        cardEl.append(cardBodyEl);
+        cardBodyEl.append(cardTitleEl);
+    
     });
 
     $("#history-container").css("margin-bottom", "20px");
